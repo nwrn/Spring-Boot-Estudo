ace5e6da8807041bece8b46739e0b008284e4691
gMatheus.dev <gmnp98@gmail.com>
2022-09-24 21:05:28 -0300

    Projeto CRUD em Spring Boot sendo desenvolvido com finalidade de estudos.

---
+++ parking-control/src/test/java/com/api/parkingcontrol/models/ParkingSpotModel.java
@@ -0,0 +1,114 @@
+package com.api.parkingcontrol.models;
+
+import javax.persistence.*;
+import  java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name= "T_PARKING_SPOT")
+public class ParkingSpotModel implements Serializable{
+    private static  final long serialVersionId = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false, unique = true, length = 10)
+    private String parkingSpotNumber;
+    @Column(nullable = false, unique = true, length = 7)
+    private String licensePlateCar;
+    @Column(nullable = false, length = 70)
+    private String brandCar;
+    @Column(nullable = false, length = 70)
+    private String modelCar;
+    @Column(nullable = false, length = 70)
+    private String colorcar;
+    @Column(nullable = false)
+    private LocalDateTime registrationDate;
+    @Column(nullable = false, length = 100)
+    private String reponsibleName;
+    @Column(nullable = false, length = 30)
+    private String apartament;
+    @Column(nullable = false, length = 30)
+    private String block;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getParkingSpotNumber() {
+        return parkingSpotNumber;
+    }
+
+    public void setParkingSpotNumber(String parkingSpotNumber) {
+        this.parkingSpotNumber = parkingSpotNumber;
+    }
+
+    public String getLicensePlateCar() {
+        return licensePlateCar;
+    }
+
+    public void setLicensePlateCar(String licensePlateCar) {
+        this.licensePlateCar = licensePlateCar;
+    }
+
+    public String getBrandCar() {
+        return brandCar;
+    }
+
+    public void setBrandCar(String brandCar) {
+        this.brandCar = brandCar;
+    }
+
+    public String getModelCar() {
+        return modelCar;
+    }
+
+    public void setModelCar(String modelCar) {
+        this.modelCar = modelCar;
+    }
+
+    public String getColorcar() {
+        return colorcar;
+    }
+
+    public void setColorcar(String colorcar) {
+        this.colorcar = colorcar;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public String getReponsibleName() {
+        return reponsibleName;
+    }
+
+    public void setReponsibleName(String reponsibleName) {
+        this.reponsibleName = reponsibleName;
+    }
+
+    public String getApartament() {
+        return apartament;
+    }
+
+    public void setApartament(String apartament) {
+        this.apartament = apartament;
+    }
+
+    public String getBlock() {
+        return block;
+    }
+
+    public void setBlock(String block) {
+        this.block = block;
+    }
+}
